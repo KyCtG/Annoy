6cecd0ba81f84f713e6f00d437307f4238eb88dd
kychitoge <133128407+KyCtG@users.noreply.github.com>
2024-11-04 19:06:57 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,10 +7,10 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor('#0099ff')
       .setTitle('Bot Commands')
-      .setDescription('▶️  **Các lệnh có sẵn:**\n‎ ')
+      .setDescription('**❖─♡️★Các lệnh hiện có :ꗄ♡️─❖**\n‎ ≪━─━─━─✮─━─━─━≫ ')
       .addFields(
-      { name: 'setup', value: 'setup một channel để bot thông báo' },
-      { name: 'announce', value: 'tạo một thông báo trên channel bạn đã setup' },
+      { name: 'setup', value: 'setup một #channel để bot thông báo' },
+      { name: 'announce', value: 'tạo một thông báo trên #channel bạn đã setup' },
         { name: 'ping', value: 'kiểm tra độ trễ của bot tùy thuộc vào khu vực' },
         { name: 'embed', value: 'một ví dụ thông báo trông như thế nào' },
     )
