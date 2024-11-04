4b83f8bf265ac9bd7060837532492a2f8aacd196
kychitoge <133128407+KyCtG@users.noreply.github.com>
2024-11-04 17:43:19 +0700

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -8,7 +8,7 @@ async function askQuestion(message, question, callback, skip = false) {
     .setColor('#0099ff')
     .setAuthor({
       name: 'Embed Message',
-      iconURL: 'https://cdn.discordapp.com/attachments/1213421081226903552/1213422313035407360/8218-alert.gif',
+      iconURL: 'https://cdn.discordapp.com/attachments/1302945490991710250/1302945590446915615/Kychitoge_1.png?ex=6729f624&is=6728a4a4&hm=523664898c1278983f09b1662a866415e2599e02f494caf6e258227934d539c0&',
       url: 'https://discord.gg/FUEHs7RCqz'
     })
     .setDescription(question);
